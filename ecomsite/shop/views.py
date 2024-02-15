e6e931b8e8dbb12514bf846cdceaa6a56b26570a
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-02-16 01:05:45 +0530

    adding and linking detail page

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -14,7 +14,15 @@ def index(request):
 
 
     # paginator code 
-    paginator = Paginator(product_objects, 2)
+    paginator = Paginator(product_objects, 4)
     page_number = request.GET.get("page")
     product_objects = paginator.get_page(page_number)
     return render(request,'shop/index.html',{"product_objects":product_objects})
+
+
+
+
+
+def detail(request,id):
+    product_object=Products.objects.get(id=id)
+    return render(request, "shop/detail.html",{'product_object':product_object})
