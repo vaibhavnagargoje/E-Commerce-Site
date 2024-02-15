4c000dab1b9c1397f283058a343dbae502396eeb
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-02-16 00:34:16 +0530

    add pegination and update view.py and index.html

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from .models import Products
-
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def index(request):
     product_objects = Products.objects.all()
+
+    # search code 
     item_name=request.GET.get('item_name')
     if item_name!= '' and item_name is not None:
         product_objects= product_objects.filter(title__icontains=item_name)
-    return render(request,'shop/index.html',{'products':product_objects})
+
+
+    # paginator code 
+    paginator = Paginator(product_objects, 2)
+    page_number = request.GET.get("page")
+    product_objects = paginator.get_page(page_number)
+    return render(request,'shop/index.html',{"product_objects":product_objects})
